5accdd1a8e26fdc94aa0fcdda1ef4d25a411311e
revatighotkar <revatighotkar1999@gmail.com>
2022-07-11 14:03:30 +0530

    assign

---
+++ HANDSON2MAY/HANDSON2MAY/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HANDSON2MAY
+{
+    internal class Program
+    {
+        class Student
+        {
+            public int ID { get; set; }
+            public string Name { get; set; }
+            public string Email { get; set; }
+
+            public static List<Student> GetStudents()
+            {
+                List<Student> studlist = new List<Student>();
+                Student a1 = new Student { ID = 101, Name = "Anjali", Email = "anjali@gmail.com" };
+                Student a2 = new Student { ID = 102, Name = "Aarati", Email = "aarati@outlook.com" };
+                Student a3 = new Student { ID = 103, Name = "Ramya", Email = "ramya@gmail.com" };
+                Student a4 = new Student { ID = 104, Name = "RaviShankar", Email = "ravi@hotmail.com" };
+                studlist.Add(a1);
+                studlist.Add(a2);
+                studlist.Add(a3);
+                studlist.Add(a4);
+                //  studlist.Add(new Student { ID = 105, Name = "Roshan", Email = "roshan@gmail.com" });
+                return studlist;
+            }
+        }
+        class QueryExpressions
+        {
+            static void Main()
+            {
+                int[] intval = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }; //array 
+
+                //query expressions to find all odd nos from the array
+                IEnumerable<int> Oddvalues = from val in intval
+                                             where (val % 3) != 0
+                                             select val;
+
+                //query expressions to find all even nos from the array
+                var evennos = from e in intval
+                              where (e % 3) == 0
+                              select e;
+
+                //IQueryable<int> queryoutput = (IQueryable<int>)(from n in intval
+                //                              where (n % 3) == 0
+                //                              select n);
+
+                //iterate the fetched results of odd nos.
+                foreach (int v in Oddvalues)
+                {
+                    Console.WriteLine(v);
+                }
+                Console.WriteLine("-----------");
+                //iterate the fetched results of even nos.
+                foreach (var v in evennos)
+                {
+                    Console.WriteLine(v);
+                }
+                //Console.WriteLine("----------");
+                ////iterate the fetched results of nos. divisible by 3
+                //foreach (int v in queryoutput)
+                //{
+                //    Console.WriteLine(v);
+                //}
+
+                //working with list objects in query expressions
+                //query the list collection and find out all students whose names start with S/R
+
+                IEnumerable<string> studnames = from stud in Student.GetStudents()
+                                                where stud.Name.Contains("A")
+                                                select stud.Name;
+
+                IEnumerable<Student> st = from a  in Student.GetStudents()
+                                          where a.ID == 104
+                                          select a;
+
+                foreach (var v in studnames)
+                {
+                    Console.WriteLine(v);
+                }
+
+                foreach (Student a in st)
+                {
+                    Console.WriteLine($"The Id :{a.ID}, Name : {a.Name} and Email :{a.Email}");
+                }
+                Console.Read();
+            }
+        }
+    }
+}
---
+++ HANDSON2MAY/HANDSON2MAY/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("HANDSON2MAY")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HANDSON2MAY")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("bf6eed22-6c31-4e1e-97d8-e3d6e98fdfcc")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
